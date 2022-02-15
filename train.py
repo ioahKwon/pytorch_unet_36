38f842fb70af28b6966e5c9ef8a7bbb924b4200e
ioahinthemac <pioneers@snu.ac.kr>
2022-02-15 21:19:02 +0900

    added.

--- train.py
+++ train.py
@@ -137,3 +137,34 @@ class UNet(nn.Module):
 
         return x
 
+## 데이터 로더를 구현하기
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, data_dir, transform =None):
+        self.data_dir = data_dir
+        self.transform = transform
+
+        lst_data = os.listdir(self.data_dir)
+
+        lst_label = [f for f in lst_data if f.startswith('label')]
+        lst_input = [f for f in lst_data if f.startswith('input')]
+
+        lst_label.sort()
+        lst_input.sort()
+
+        self.lst_label = lst_label
+        self.lst_input = lst_input
+
+    def __len__(self):
+        return len(self.lst_label)
+
+    def __getitem__(selfself, index):
+        label = np.load(os.path.join(self.data_dir, self.lst_label[index]))
+        input = np.load(os.path.join(self.data_dir, self.lst_input[index]))
+
+        label = label/255.0
+        input = input/255.0
+
+        if label.ndim == 2:
+            label = label[:,:, np.newaxis]
+
+
