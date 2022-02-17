7cc5cd6ee30a37513706d1853ad5145a78ebda35
ioahinthemac <pioneers@snu.ac.kr>
2022-02-17 20:58:44 +0900

    added.

--- train.py
+++ train.py
@@ -262,10 +262,10 @@ plt.show()
 transform = transforms.Compose([Normalization(mean=0.5, std=0.5), RandomFlip(), ToTensor()])
 
 dataset_train = Dataset(data_dir=os.path.join(data_dir, 'train'), transform=transform)
-loader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=8)
+loader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=0)
 
 dataset_val = Dataset(data_dir=os.path.join(data_dir, 'val'), transform=transform)
-loader_val = DataLoader(dataset_val, batch_size=batch_size, shuffle=True, num_workers=8)
+loader_val = DataLoader(dataset_val, batch_size=batch_size, shuffle=True, num_workers=0)
 
 ## 네트워크 생성하기
 net = UNet().to(device)
@@ -286,7 +286,7 @@ num_batch_val = np.ceil(num_data_val / batch_size)
 ## 그밖에 부수적인 functions 설정하기
 fn_tonumpy = lambda x: x.to('cpu').detach().numpy().transpose(0, 2, 3, 1)
 fn_denorm = lambda x, mean, std: (x * std) + mean
-fn_class = lambda x: 1.0 * (x > 0.5)
+fn_class = lambda x: 1.0 * (x > 0.5) # classification() using thresholding(p=0.5)
 
 ## Tensorboard 를 사용하기 위한 SummaryWriter 설정
 writer_train = SummaryWriter(log_dir=os.path.join(log_dir, 'train'))
@@ -374,12 +374,12 @@ for epoch in range(st_epoch + 1, num_epoch + 1):
 
             loss_arr += [loss.item()]
 
-            print('VALID: EPOCH %O4d / %04d | BATCH %04d / %04d | LOSS %.4f'
-                  %(epoch, num_epoch, batch, batch_size, np.mean(loss_arr)))
+            print("VALID: EPOCH %O4d / %04d | BATCH %04d / %04d | LOSS %.4f" %
+                  (epoch, num_epoch, batch, num_batch_val, np.mean(loss_arr)))
 
             # Tensorboard 저장하기
             label = fn_tonumpy(label)
-            input = fn_tonumpy(fn_denorm(input))
+            input = fn_tonumpy(fn_denorm(input, mean=0.5, std=0.5))
             output = fn_tonumpy(fn_class(output))
 
             writer_val.add_image('label', label, num_batch_val * (epoch - 1) + batch, dataformats='NHWC')
@@ -388,11 +388,15 @@ for epoch in range(st_epoch + 1, num_epoch + 1):
 
     writer_val.add_scalar('loss', np.mean(loss_arr), epoch)
 
-    if epoch % 5 == 0:
+    if epoch % 50 == 0:
         save(ckpt_dir=ckpt_dir, net=net, optim=optim, epoch=epoch)
 
 writer_train.close()
 writer_val.close()
+
+
+##
+##
 
 
 ##
